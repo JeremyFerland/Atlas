859e6bb0a2faada0a05937a869ca098ef2ca8b10
jeremyferland <jeremyferland@gmail.com>
2016-04-13 22:13:36 -0400

    roads

---
+++ Visuel/Prototype/unity/Assets/RoadCreator.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadCreator : MonoBehaviour {
+
+	public Vector2[] roadPts;
+	public GameObject roadPrefab;
+	public GameObject corner;
+	cylindre[] roads;
+	public bool isRoadStart = false;
+	public bool isRoadFinish = false;
+	int index = -1;
+
+
+	public void StartRoad (){
+		isRoadStart = true;
+		nextRoad ();
+	}
+
+	public void DestroyRoad(){
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+		roads = new cylindre[roadPts.Length];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.S)) {
+			StartRoad();
+		}
+
+		if (isRoadStart && !isRoadFinish) {
+			if(roads[index].isFinish){
+				nextRoad();
+			}
+		}
+	
+	}
+	
+	void nextRoad(){
+		index++;
+		if (index >= roadPts.Length -1) {
+			isRoadFinish = true;
+			return;
+		}
+		GameObject tempCorner = GameObject.Instantiate (corner) as GameObject;
+		tempCorner.transform.parent = this.transform;
+		tempCorner.transform.position = roadPts [index];
+		GameObject tempNextRoad = GameObject.Instantiate(roadPrefab)as GameObject;
+		tempNextRoad.transform.parent = this.transform;
+		cylindre cylindre = tempNextRoad.GetComponent<cylindre> ();
+		tempNextRoad.transform.position = roadPts [index];
+		cylindre.initialPosition = roadPts[index];
+		cylindre.targetPosition = roadPts[index+1];
+		cylindre.isCreating = true;
+		roads [index] = cylindre;
+		tempCorner.transform.localScale = new Vector3 (1, 1, 1);
+	}
+}
---
+++ Visuel/Prototype/unity/Assets/cylindre.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cylindre : MonoBehaviour {
+
+	public bool isCreating = false;
+	public Vector2 targetPosition;
+	public Vector2 initialPosition;
+	public bool isFinish=false;
+	float removeThatToScale;
+	Vector3 eulerAngle = new Vector3 (-1000,0,0);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isCreating) {
+			if(initialPosition.x != targetPosition.x){
+				if(eulerAngle == new Vector3 (-1000,0,0)){
+					transform.localEulerAngles = new Vector3(0,0,270);
+					eulerAngle = transform.localEulerAngles;
+					removeThatToScale = Mathf.Abs(initialPosition.x);
+				}
+				float scaleX = Mathf.MoveTowards(initialPosition.x, targetPosition.x, 0.05f);
+				if(scaleX-removeThatToScale == 0){
+					return;
+				}
+				initialPosition = new Vector2(scaleX,initialPosition.y);
+				transform.localScale = new Vector3(1,scaleX-removeThatToScale,1);
+				if(Mathf.Round(initialPosition.x)  == Mathf.Round(targetPosition.x)){
+					isFinish = true;
+				}
+
+			}else{
+				if(eulerAngle == new Vector3 (-1000,0,0)){
+					transform.localEulerAngles = new Vector3(0,0,0);
+					eulerAngle = transform.localEulerAngles;	
+					removeThatToScale = Mathf.Abs(initialPosition.y);
+				}
+				float scaleY = Mathf.MoveTowards(initialPosition.y, targetPosition.y, 0.05f);
+				initialPosition = new Vector2(initialPosition.x,scaleY);
+				if(scaleY-removeThatToScale == 0){
+					return;
+				}
+				transform.localScale = new Vector3(1,scaleY-removeThatToScale,1);
+				if(Mathf.Round(initialPosition.y * 10)  == Mathf.Round(targetPosition.y * 10)){
+					isFinish = true;
+				}
+			}
+		
+		}
+
+	}
+}
